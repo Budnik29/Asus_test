62cc39c8a2324f8ab2967e99c752726c4f3ca9a5
budnik2905 <budnik2905@gmail.com>
2022-12-20 02:53:21 +0300

    test

---
+++ 3.py
@@ -0,0 +1,6 @@
+Python 3.10.6 (main, Nov 14 2022, 16:10:14) [GCC 11.3.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+print('Манечка')
+Манечка
+print('test')
+print('test')
